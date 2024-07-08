844f71bd62a41d59974b91af4c260396fbdd0fdb
Abdullah Zafar <136666960+AbdullahGZafar@users.noreply.github.com>
2024-07-08 12:21:40 -0400

    Add files

---
+++ script.js
@@ -0,0 +1,26 @@
+function add(Array){
+}
+
+function subtract(Array){
+
+}
+
+function multiply(Array){
+
+}
+
+function divide(Array){
+
+}
+
+function factorial(Num){
+
+}
+
+function power(Num){
+
+}
+
+function operate(Num1, Num2){
+
+}
