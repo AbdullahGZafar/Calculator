088ca87ce95693c66bf52bdb122ad96d63cfb352
Abdullah Zafar <136666960+AbdullahGZafar@users.noreply.github.com>
2024-07-16 21:21:43 -0400

    add operator buttons

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ const allOperatorButtons = document.querySelector(".operator-buttons-container")
 
 //all number buttons can be accessed with number[x]; 0 is at index 0, 1 at 1, etc.
 const number = Array.from(document.querySelectorAll(".number-buttons"));
-const operator = Array.from(document.querySelectorAll(".operator-buttons"));
 
+const plusButton = querySelector("#+");
+const minusButton = querySelector("#-");
+const multiplyButton = querySelector("#x");
+const divideButton = querySelector("#/");
+const factorialButton = querySelector("#!");
+const squareRootButton = querySelector("#√");
+const equalsButton = querySelector("#=");
 
 function add() {
   const result = Array.from(arguments).reduce(function (total, sum) {
